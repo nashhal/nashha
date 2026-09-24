d5d561e249ffd301af750f839e5a1a65ba745389
nashhal <husseinalyfai@gmail.com>
2026-09-24 21:07:18 +0300

    Fix service worker shell after English cleanup

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE='southern-revolution-shell-v9';
+const CACHE='al-thawra-shell-v10';
 const DATA='nashhal-data-v2';
-const SHELL=['./','./index.html','./article.html','./about.html','./editorial-policy.html','./ai-policy.html','./corrections.html','./trust.html','./web3.html','./search.html','./language.js','./global-ui.js','./news-loader.js','./bilingual-news.js','./logo.jpg','./manifest.webmanifest','./favicon.svg'];
+const SHELL=['./','./index.html','./article.html','./about.html','./editorial-policy.html','./ai-policy.html','./corrections.html','./trust.html','./web3.html','./search.html','./news-loader.js','./manifest.webmanifest','./favicon.svg'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));
