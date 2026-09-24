8286333f427c2f08217be7d31c757e243baf3601
nashhal <husseinalyfai@gmail.com>
2026-09-24 15:40:49 +0300

    Complete Southern Revolution brand migration

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='nashhal-shell-v4';
+const CACHE='southern-revolution-shell-v5';
 const DATA='nashhal-data-v2';
 const SHELL=['./','./index.html','./article.html','./about.html','./editorial-policy.html','./ai-policy.html','./corrections.html','./trust.html','./web3.html','./search.html','./language.js','./global-ui.js','./news-loader.js','./bilingual-news.js','./logo.jpg','./manifest.webmanifest'];
 
