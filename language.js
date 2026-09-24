19d846b207ebdfb2fac5549099205e1d761fa56f
nashhal <husseinalyfai@gmail.com>
2026-09-24 21:07:21 +0300

    Remove obsolete language toggle layer

--- language.js
+++
@@ -1,5 +0,0 @@
-(function(){'use strict';
-const KEY='nashhal-language';
-window.NashhalLang={get:()=> 'en',toggle:()=>{} };
-document.addEventListener('DOMContentLoaded',()=>{document.documentElement.lang='en';document.documentElement.dir='ltr';});
-})();
