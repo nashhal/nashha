924c94de3b524d2890b6030cb637f492c57dc7b0
nashhal <husseinalyfai@gmail.com>
2026-09-24 21:42:25 +0300

    Cache paper edition assets and refresh shell version

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE='al-thawra-shell-v11';
+const CACHE='al-thawra-shell-v12';
 const DATA='nashhal-data-v2';
-const SHELL=['./','./index.html','./article.html','./about.html','./editorial-policy.html','./ai-policy.html','./corrections.html','./trust.html','./web3.html','./search.html','./news-loader.js','./manifest.webmanifest','./favicon.svg'];
+const SHELL=['./','./index.html','./article.html','./about.html','./editorial-policy.html','./ai-policy.html','./corrections.html','./trust.html','./web3.html','./search.html','./news-loader.js','./manifest.webmanifest','./favicon.svg','./paper-effects.css','./paper-effects.js'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));
